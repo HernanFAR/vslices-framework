534c1f0714694b3f8a8acea892f6e1d8ebe78577
Hernan <h.f.alvarez.r@gmail.com>
2023-06-03 22:37:46 -0400

    FEAT: Se creo el behavior de validación

---
+++ VSlices.CrossCutting.Validation.FluentValidation/FluentValidationBehavior.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using OneOf;
+using OneOf.Types;
+using VSlices.Core.Abstracts.BusinessLogic;
+using VSlices.Core.Abstracts.Responses;
+
+namespace VSlices.CrossCutting.Validation.FluentValidation;
+
+public class FluentValidationBehavior<TRequest, TResponse> : AbstractValidationBehavior<TRequest, TResponse> 
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IValidator<TRequest>? _requestValidator;
+
+    public FluentValidationBehavior(IEnumerable<IValidator<TRequest>> requestValidators)
+    {
+        _requestValidator = requestValidators.FirstOrDefault();
+    }
+
+    protected override async ValueTask<OneOf<Success, BusinessFailure>> ValidateAsync(TRequest request, CancellationToken cancellationToken = default)
+    {
+        if (_requestValidator is null)
+        {
+            return new Success();
+        }
+
+        var requestValidationResult = await _requestValidator.ValidateAsync(request, cancellationToken);
+
+        if (requestValidationResult.IsValid) return new Success();
+
+        var errors = requestValidationResult
+            .Errors.Select(e => e.ErrorMessage)
+            .ToArray();
+
+        return BusinessFailure.Of.Validation(errors);
+    }
+}
