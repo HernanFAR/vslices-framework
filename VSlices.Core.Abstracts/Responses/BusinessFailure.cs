dbf2c8ea9b9c61a0ead392fe6a63db189643b410
Hernan <h.f.alvarez.r@gmail.com>
2023-07-04 13:00:12 -0400

    FEAT: Agregado de licencia, correción de "Copyright", agregado de IHandler<TRequest> y se agregó la definición inicial AbstractExceptionHandlingBehabior

--- VSlices.Core.Abstracts/Responses/BusinessFailure.cs
+++ VSlices.Core.Abstracts/Responses/BusinessFailure.cs
@@ -7,7 +7,8 @@ public enum FailureKind
     ConcurrencyError,
     ContractValidation,
     DomainValidation,
-    DefaultError
+    DefaultError,
+    UnmanagedException
 }
 
 public readonly record struct BusinessFailure(FailureKind Kind, string[] Errors)
@@ -20,12 +21,14 @@ public readonly record struct BusinessFailure(FailureKind Kind, string[] Errors)
         public static BusinessFailure ConcurrencyError(string[] errors) => new(FailureKind.ConcurrencyError, errors);
         public static BusinessFailure ConcurrencyError() => new(FailureKind.ConcurrencyError, Array.Empty<string>());
         public static BusinessFailure ContractValidation(string[] errors) => new(FailureKind.ContractValidation, errors);
-        public static BusinessFailure ContractValidation(string error) => new(FailureKind.ContractValidation, new []{ error });
+        public static BusinessFailure ContractValidation(string error) => new(FailureKind.ContractValidation, new[] { error });
         public static BusinessFailure DomainValidation(string[] errors) => new(FailureKind.DomainValidation, errors);
-        public static BusinessFailure DomainValidation(string error) => new(FailureKind.DomainValidation, new []{ error });
+        public static BusinessFailure DomainValidation(string error) => new(FailureKind.DomainValidation, new[] { error });
         public static BusinessFailure DefaultError(string[] errors) => new(FailureKind.DefaultError, errors);
-        public static BusinessFailure DefaultError(string error) => new(FailureKind.DefaultError, new []{ error });
+        public static BusinessFailure DefaultError(string error) => new(FailureKind.DefaultError, new[] { error });
         public static BusinessFailure DefaultError() => new(FailureKind.DefaultError, Array.Empty<string>());
+        public static BusinessFailure UnhandledException() => new(FailureKind.UnmanagedException, Array.Empty<string>());
+
     }
 }
 
---
+++ VSlices.CrossCutting.ExceptionHandling/AbstractExceptionHandlingBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.ExceptionServices;
+using OneOf;
+using VSlices.Core.Abstracts.BusinessLogic;
+using VSlices.Core.Abstracts.Responses;
+
+namespace VSlices.CrossCutting.ExceptionHandling;
+
+public abstract class AbstractExceptionHandlingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TRequest> 
+    where TRequest : IRequest<TRequest>
+{
+    public async ValueTask<OneOf<TRequest, BusinessFailure>> HandleAsync(TRequest request, RequestHandlerDelegate<TRequest> next, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception ex)
+        {
+            await ProcessExceptionAsync(ex);
+
+            return BusinessFailure.Of.UnhandledException();
+        }
+    }
+
+    protected abstract ValueTask ProcessExceptionAsync(Exception ex);
+}
