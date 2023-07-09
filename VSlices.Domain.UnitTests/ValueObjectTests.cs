2c12c86895635057a956ca220ef35f5fa4ae51f7
Hernan <h.f.alvarez.r@gmail.com>
2023-07-09 14:42:01 -0400

    FEAT: Se agregaron clases base para emplear Domain Driven Development

---
+++ VSlices.Domain.UnitTests/ValueObjectTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace VSlices.Domain.UnitTests;
+
+public class ValueObjectTests
+{
+    public class FirstValueObject : ValueObject
+    {
+        public int Number { get; }
+
+        public FirstValueObject(int number)
+        {
+            Number = number;
+        }
+
+        protected override IEnumerable<object?> GetAtomicValues()
+        {
+            yield return Number;
+        }
+    }
+
+    public class SecondValueObject : ValueObject
+    {
+        public int Number { get; }
+
+        public SecondValueObject(int number)
+        {
+            Number = number;
+        }
+
+        protected override IEnumerable<object?> GetAtomicValues()
+        {
+            yield return Number;
+        }
+    }
+
+    [Fact]
+    public void ValueEquals_ShouldReturnFalse_DetailDifferentType()
+    {
+        // Arrange
+        const int number = 1;
+
+        var firstValueObject = new FirstValueObject(number);
+        var secondValueObject = new SecondValueObject(number);
+
+        // Assert
+        firstValueObject.ValueEquals(secondValueObject).Should().BeFalse();
+    }
+
+    // test ValueEquals with same type but different values
+    [Fact]
+    public void ValueEquals_ShouldReturnFalse_DetailDifferentValues()
+    {
+        // Arrange
+        const int number1 = 1;
+        const int number2 = 2;
+
+        var firstValueObject = new FirstValueObject(number1);
+        var secondValueObject = new FirstValueObject(number2);
+
+        // Assert
+        firstValueObject.ValueEquals(secondValueObject).Should().BeFalse();
+    }
+
+    // test ValueEquals with same type and same values  
+    [Fact]
+    public void ValueEquals_ShouldReturnTrue_DetailSameValues()
+    {
+        // Arrange
+        const int number = 1;
+
+        var firstValueObject = new FirstValueObject(number);
+        var secondValueObject = new FirstValueObject(number);
+
+        // Assert
+        firstValueObject.ValueEquals(secondValueObject).Should().BeTrue();
+    }
+
+}
---
+++ VSlices.Domain/ValueObject.cs
@@ -0,0 +1,35 @@
+﻿namespace VSlices.Domain;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object?> GetAtomicValues();
+
+    public bool ValueEquals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+
+        using var thisValues = GetAtomicValues().GetEnumerator();
+        using var otherValues = other.GetAtomicValues().GetEnumerator();
+
+        while (thisValues.MoveNext() && otherValues.MoveNext())
+        {
+            if (thisValues.Current is null ^ otherValues.Current is null)
+            {
+                return false;
+            }
+
+            if (thisValues.Current != null &&
+                !thisValues.Current.Equals(otherValues.Current))
+            {
+                return false;
+            }
+        }
+
+        return !thisValues.MoveNext() && !otherValues.MoveNext();
+    }
+}
