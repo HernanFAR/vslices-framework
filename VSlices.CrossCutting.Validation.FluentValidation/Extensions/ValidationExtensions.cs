88bf50a32d95d0cbd8c55c5515d2dea228aa72de
Hernan <h.f.alvarez.r@gmail.com>
2023-07-17 08:36:16 -0400

    FEAT: Funcion de extensión para agregar facilmente el FluentValidationBehavior

---
+++ VSlices.CrossCutting.Validation.FluentValidation/Extensions/ValidationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using VSlices.CrossCutting.Validation.FluentValidation;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ValidationExtensions
+{
+    /// <summary>
+    /// Adds the FluentValidationBehavior to the pipeline.
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="lifetime">Service lifetime</param>
+    /// <returns>Service collection</returns>
+    public static IServiceCollection AddFluentValidationBehavior(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+    {
+        services.AddPipelineBehavior(typeof(FluentValidationBehavior<,>), lifetime);
+        return services;
+    }
+}
