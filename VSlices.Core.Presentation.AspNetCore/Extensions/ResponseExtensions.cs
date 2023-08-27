6af9de1531dbe06564d6f1306281d72b491bc031
Hernan <h.f.alvarez.r@gmail.com>
2023-08-27 01:50:11 -0400

    FEAT: Se agrego una conversión del Business Failure en el ProblemDetails (Implementación RFC 9457)

--- VSlices.Core.Presentation.AspNetCore/Extensions/ResponseExtensions.cs
+++ VSlices.Core.Presentation.AspNetCore/Extensions/ResponseExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 // ReSharper disable once CheckNamespace
 namespace VSlices.Core.Abstracts.Responses;
@@ -7,10 +8,11 @@ public static class ResponseExtensions
 {
     /// <summary>
     /// Maps a <see cref="Response{TSuccess}"/> to a <see cref="IResult"/>, using the provided function in success case.
+    /// <para>It uses the <see href="https://datatracker.ietf.org/doc/html/rfc9457"/></para>
     /// </summary>
-    /// <typeparam name="TSuccess"></typeparam>
-    /// <param name="result"></param>
-    /// <param name="successFunc"></param>
+    /// <typeparam name="TSuccess">Return type in success</typeparam>
+    /// <param name="result">Result</param>
+    /// <param name="successFunc">Function to execute in </param>
     /// <returns>The <see cref="IResult"/> of the use case</returns>
     /// <exception cref="ArgumentOutOfRangeException"></exception>
     public static IResult MatchEndpointResult<TSuccess>(this Response<TSuccess> result,
@@ -19,25 +21,101 @@ public static class ResponseExtensions
         if (result.IsSuccess)
             return successFunc(result.SuccessValue);
 
-        var businessFailure = result.BusinessFailure;
+        var bf = result.BusinessFailure;
 
-        return businessFailure.Kind switch
+        return bf.Kind switch
         {
-            FailureKind.NotAllowedUser => TypedResults.Forbid(),
-            FailureKind.NotFoundResource => businessFailure.Errors.Any()
-                ? TypedResults.NotFound(businessFailure.Errors)
-                : TypedResults.NotFound(),
-            FailureKind.ConcurrencyError => businessFailure.Errors.Any()
-                ? TypedResults.Conflict(businessFailure.Errors)
-                : TypedResults.Conflict(),
-            FailureKind.ContractValidation => TypedResults.UnprocessableEntity(businessFailure.Errors),
-            FailureKind.DomainValidation => TypedResults.UnprocessableEntity(businessFailure.Errors),
-            FailureKind.DefaultError => businessFailure.Errors.Any()
-                ? TypedResults.BadRequest(businessFailure.Errors)
-                : TypedResults.BadRequest(),
-            FailureKind.NotAuthenticatedUser => TypedResults.Unauthorized(),
-            FailureKind.UnhandledException => TypedResults.StatusCode(StatusCodes.Status500InternalServerError),
-            _ => throw new ArgumentOutOfRangeException(nameof(businessFailure.Kind), "A not valid FailureKind value was returned")
+            FailureKind.Unspecified => bf.Errors.Any()
+                ? TypedResults.Problem(new HttpValidationProblemDetails(bf.Errors.ToDictionary())
+                {
+                    Status = StatusCodes.Status400BadRequest,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a client error."
+                })
+                : TypedResults.Problem(new HttpValidationProblemDetails
+                {
+                    Status = StatusCodes.Status400BadRequest,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a client error."
+                }),
+            FailureKind.NotAuthenticatedUser => bf.Errors.Any()
+                ? TypedResults.Problem(new HttpValidationProblemDetails(bf.Errors.ToDictionary())
+                {
+                    Status = StatusCodes.Status401Unauthorized,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a authentication error."
+                })
+                : TypedResults.Problem(new HttpValidationProblemDetails
+                {
+                    Status = StatusCodes.Status401Unauthorized,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a authentication error."
+                }),
+            FailureKind.NotAllowedUser => bf.Errors.Any()
+                ? TypedResults.Problem(new HttpValidationProblemDetails(bf.Errors.ToDictionary())
+                {
+                    Status = StatusCodes.Status403Forbidden,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a authentication error."
+                })
+                : TypedResults.Problem(new HttpValidationProblemDetails
+                {
+                    Status = StatusCodes.Status403Forbidden,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a authentication error."
+                }),
+            FailureKind.NotFoundResource => bf.Errors.Any()
+                ? TypedResults.Problem(new HttpValidationProblemDetails(bf.Errors.ToDictionary())
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The requested resource was not found."
+                })
+                : TypedResults.Problem(new HttpValidationProblemDetails
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The requested resource was not found."
+                }),
+            FailureKind.ConcurrencyError => bf.Errors.Any()
+                ? TypedResults.Problem(new HttpValidationProblemDetails(bf.Errors.ToDictionary())
+                {
+                    Status = StatusCodes.Status409Conflict,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a concurrency error."
+                })
+                : TypedResults.Problem(new HttpValidationProblemDetails
+                {
+                    Status = StatusCodes.Status409Conflict,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a concurrency error."
+                }),
+            FailureKind.ContractValidation => TypedResults.Problem(new HttpValidationProblemDetails(bf.Errors.ToDictionary())
+                {
+                    Status = StatusCodes.Status422UnprocessableEntity,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of one or more validation errors."
+                }),
+            FailureKind.DomainValidation => TypedResults.Problem(new HttpValidationProblemDetails(bf.Errors.ToDictionary())
+                {
+                    Status = StatusCodes.Status422UnprocessableEntity,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of one or more validation errors."
+                }),
+            FailureKind.UnhandledException => bf.Errors.Any()
+                ? TypedResults.Problem(new HttpValidationProblemDetails(bf.Errors.ToDictionary())
+                {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a server error."
+                })
+                : TypedResults.Problem(new HttpValidationProblemDetails
+                {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Detail = bf.Detail,
+                    Title = bf.Title ?? "The request could not be processed because of a server error."
+                }),
+            _ => throw new ArgumentOutOfRangeException(nameof(bf.Kind), "A not valid FailureKind value was returned")
         };
     }
 }
---
+++ VSlices.Core.Presentation.AspNetCore/Extensions/ValidationErrorArrayExtensions.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable once CheckNamespace
+namespace VSlices.Core.Abstracts.Responses;
+
+public static class ValidationErrorArrayExtensions
+{
+    /// <summary>
+    /// Converts a <see cref="ValidationError"/> array to a dictionary
+    /// </summary>
+    /// <param name="errors">Validation error array</param>
+    /// <returns>A dictionary with the property name as key and the errors as values</returns>
+    public static Dictionary<string, string[]> ToDictionary(this ValidationError[] errors)
+    {
+        var dictionary = new Dictionary<string, string[]>();
+        var propertyNames = errors.Select(x => x.Name).Distinct();
+
+        foreach (var propertyName in propertyNames)
+        {
+            dictionary.Add(propertyName, errors.Where(x => x.Name == propertyName).Select(e => e.Detail).ToArray());
+        }
+
+        return dictionary;
+    }
+}
