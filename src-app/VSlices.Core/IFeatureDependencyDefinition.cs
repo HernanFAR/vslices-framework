5664ec7bb612f9cf26bb7d9e7077b5359c3258a7
Hernan <h.f.alvarez.rubio>
2024-01-10 21:57:35 -0300

    feat: added VSlices.Core app

---
+++ src-app/VSlices.Core/IFeatureDependencyDefinition.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace VSlices.Core;
+
+/// <summary>
+/// Specifies dependencies in a given use case
+/// </summary>
+public interface IFeatureDependencyDefinition
+{
+    /// <summary>
+    /// Defines the dependencies for the use case
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    static abstract void DefineDependencies(IServiceCollection services);
+}
---
+++ src-app/VSlices.Core/IHandler.cs
@@ -0,0 +1,33 @@
+﻿using VSlices.Base;
+using VSlices.Base.Responses;
+
+namespace VSlices.Core;
+
+/// <summary>
+/// Defines a handler for a <see cref="IBaseRequest{TResult}"/>
+/// </summary>
+/// <remarks>If idempotency is necessary, the handler itself must ensure it</remarks>
+/// <typeparam name="TRequest">The request to be handled</typeparam>
+/// <typeparam name="TResult">The expected response of the handler</typeparam>
+public interface IHandler<in TRequest, TResult>
+    where TRequest : IBaseRequest<TResult>
+{
+    /// <summary>
+    /// Handles the request
+    /// </summary>
+    /// <param name="request">The request to be handled</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>
+    /// A <see cref="ValueTask{T}"/> that represents an asynchronous operation which returns a <see cref="Result{T}"/>
+    /// of <see cref="Success"/> that represents the result of the operation
+    /// </returns>
+    ValueTask<Result<TResult>> HandleAsync(TRequest request, CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// Defines a handler for a <see cref="IBaseRequest{TResult}"/>
+/// </summary>
+/// <typeparam name="TRequest">The request to be handled</typeparam>
+public interface IHandler<in TRequest> : IHandler<TRequest, Success>
+    where TRequest : IBaseRequest<Success>
+{ }
