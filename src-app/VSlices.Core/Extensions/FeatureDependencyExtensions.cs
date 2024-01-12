517f78380642d9d88a65e50228e7c44113a20df9
Hernan <h.f.alvarez.rubio>
2024-01-11 22:27:58 -0300

    feat: added extensions to VSlices.Core

---
+++ src-app/VSlices.Core/Extensions/FeatureDependencyExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System.Reflection;
+using VSlices.Core;
+// ReSharper disable CheckNamespace
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// <see cref="IServiceCollection"/> extensions for <see cref="IFeatureDependencyDefinition"/>
+/// </summary>
+public static class FeatureDependencyExtensions
+{
+    /// <summary>
+    /// Adds the dependencies defined in the <see cref="IFeatureDependencyDefinition"/> implementations from the
+    /// <see cref="Assembly"/>'s <typeparamref name="TAnchor"></typeparamref>
+    /// </summary>
+    /// <typeparam name="TAnchor"></typeparam>
+    /// <param name="services">Service collection</param>
+    /// <returns>Service collection</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static IServiceCollection AddFeatureDependenciesFromAssemblyContaining<TAnchor>(this IServiceCollection services)
+    {
+        return services.AddFeatureDependenciesFromAssembly(typeof(TAnchor).Assembly);
+    }
+
+    /// <summary>
+    /// Adds the dependencies defined in the <see cref="IFeatureDependencyDefinition"/> implementations from the
+    /// <see cref="Assembly"/>'s specified <see cref="Type"/>
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="services">Service collection</param>
+    /// <returns>Service collection</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static IServiceCollection AddFeatureDependenciesFromAssemblyContaining(this IServiceCollection services,
+        Type type)
+    {
+        return services.AddFeatureDependenciesFromAssembly(type.Assembly);
+    }
+
+    /// <summary>
+    /// Adds the dependencies defined in the <see cref="IFeatureDependencyDefinition"/> implementations from the specified
+    /// <see cref="Assembly"/>
+    /// </summary>
+    /// <param name="assembly">Assembly to scan</param>
+    /// <param name="services">Service collection</param>
+    /// <returns>Service collection</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static IServiceCollection AddFeatureDependenciesFromAssembly(this IServiceCollection services,
+        Assembly assembly)
+    {
+        var types = assembly.ExportedTypes
+            .Where(e => typeof(IFeatureDependencyDefinition).IsAssignableFrom(e))
+            .Where(e => e is { IsAbstract: false, IsInterface: false });
+
+        foreach (var type in types)
+        {
+            services.AddFeatureDependency(type);
+        }
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds the dependencies defined in the <typeparamref name="T"></typeparamref> implementation
+    /// </summary>
+    /// <typeparam name="T"><see cref="IFeatureDependencyDefinition"/> implementation</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <returns>Service collection</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static IServiceCollection AddFeatureDependency<T>(this IServiceCollection services)
+        where T : IFeatureDependencyDefinition
+    {
+        T.DefineDependencies(services);
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds the dependencies defined in the <see cref="IFeatureDependencyDefinition"/> implementations from the
+    /// specified <see cref="Type"/>
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="type"><see cref="IFeatureDependencyDefinition"/> implementation</param>
+    /// <returns>Service collection</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static IServiceCollection AddFeatureDependency(this IServiceCollection services,
+        Type type)
+    {
+        if (!typeof(IFeatureDependencyDefinition).IsAssignableFrom(type))
+        {
+            throw new InvalidOperationException(
+                $"{type.FullName} does not implement {nameof(IFeatureDependencyDefinition)}");
+        }
+
+        var defineDependenciesMethod = type.GetMethod(nameof(IFeatureDependencyDefinition.DefineDependencies));
+
+        defineDependenciesMethod!.Invoke(null, new object?[] { services });
+
+        return services;
+    }
+}
---
+++ src-app/VSlices.Core/Extensions/HandlerExtensions.cs
@@ -0,0 +1,46 @@
+﻿using VSlices.Core;
+// ReSharper disable CheckNamespace
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// <see cref="IServiceCollection"/> extensions for <see cref="IHandler{TRequest,TResult}"/>
+/// </summary>
+public static class HandlerExtensions
+{
+    /// <summary>
+    /// Adds <typeparamref name="T"/> as <see cref="IHandler{TRequest,TResult}"/> to the service collection.
+    /// </summary>
+    /// <typeparam name="T">The endpoint definition to be added</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <returns>Service collection</returns>
+    public static IServiceCollection AddHandler<T>(this IServiceCollection services)
+    {
+        return services.AddHandler(typeof(T));
+    }
+
+    /// <summary>
+    /// Adds an the specified <see cref="Type"/> as <see cref="IHandler{TRequest,TResult}"/> to the service collection.
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="handlerType">The endpoint definition to be added</param>
+    /// <exception cref="InvalidOperationException"></exception>
+    /// <returns>Service collection</returns>
+    public static IServiceCollection AddHandler(this IServiceCollection services,
+        Type handlerType)
+    {
+        var handlerInterface = handlerType.GetInterfaces()
+            .Where(o => o.IsGenericType)
+            .SingleOrDefault(o => o.GetGenericTypeDefinition() == typeof(IHandler<,>));
+
+        if (handlerInterface is null)
+        {
+            throw new InvalidOperationException(
+                $"The type {handlerType.FullName} does not implement {typeof(IHandler<,>).FullName}");
+        }
+
+        services.AddTransient(handlerInterface, handlerType);
+
+        return services;
+    }
+}
