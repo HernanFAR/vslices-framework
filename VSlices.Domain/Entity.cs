5be0066eda6a7d6e9903904acbc8097eac4536c6
Hernan <h.f.alvarez.rubio>
2024-01-14 00:44:22 -0300

    feat: added VSlices.Domain

---
+++ VSlices.Domain/Entity.cs
@@ -0,0 +1,70 @@
+﻿namespace VSlices.Domain;
+
+/// <summary>
+/// Defines an entity with non specified keys
+/// </summary>
+public interface IEntity
+{
+    /// <summary>
+    /// Gets the keys of the entity
+    /// </summary>
+    /// <returns>An array with the keys of the entity</returns>
+    object[] GetKeys();
+
+    /// <summary>
+    /// Performs a identity check between two entities
+    /// </summary>
+    /// <param name="other">The other entity to </param>
+    /// <returns>true if is equal, false if not</returns>
+    bool EntityEquals(IEntity? other);
+}
+
+/// <summary>
+/// Base entity with non specified keys
+/// </summary>
+/// <remarks>Provides a better <see cref="ToString()"/> implementation, aside of a <see cref="GetKeys()"/> and an <see cref="EntityEquals"/> method</remarks>
+public abstract class Entity : IEntity
+{
+    /// <inheritdoc/>
+    public override string ToString() => this.EntityToString();
+
+    /// <inheritdoc/>
+    public abstract object[] GetKeys();
+
+    /// <inheritdoc/>
+    public bool EntityEquals(IEntity? other) => this.EntityEqualsTo(other);
+}
+
+/// <summary>
+/// Base entity with a strong typed key
+/// </summary>
+/// <remarks>Provides a better <see cref="Entity.ToString()"/> implementation, aside of a <see cref="GetKeys()"/> and an <see cref="Entity.EntityEquals"/> method</remarks>
+public abstract class Entity<TKey> : Entity
+{
+    /// <summary>
+    /// The key of the entity
+    /// </summary>
+    public TKey Id { get; private set; }
+
+    /// <summary>
+    /// Empty constructor to use in serialization scenarios
+    /// </summary>
+    /// <remarks>Do not use this constructor in your code, if is not for serialization</remarks>
+    protected Entity()
+    {
+        Id = default!;
+    }
+
+    /// <summary>
+    /// Creates a new entity with the specified key
+    /// </summary>
+    /// <param name="id">The key of the entity</param>
+    protected Entity(TKey id)
+    {
+        Id = id;
+    }
+
+    /// <inheritdoc/>
+    public override object[] GetKeys() => new object[] { Id! };
+
+}
---
+++ VSlices.Domain/ValueObject.cs
@@ -0,0 +1,49 @@
+﻿namespace VSlices.Domain;
+
+/// <summary>
+/// Base class for value objects
+/// </summary>
+/// <remarks> Provides a <see cref="ValueEquals"/> method</remarks>
+public abstract class ValueObject
+{
+    /// <summary>
+    /// Gets the values of the value object
+    /// </summary>
+    /// <remarks>Use <code>yield return val;</code></remarks>
+    /// <returns>An enumeration of the properties</returns>
+    protected abstract IEnumerable<object?> GetAtomicValues();
+
+    /// <summary>
+    /// Performs a value check between two value objects
+    /// </summary>
+    /// <param name="obj">The other value object to compare with</param>
+    /// <returns>true if it has the same values, false if not</returns>
+    public bool ValueEquals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+
+        using var thisValues = GetAtomicValues().GetEnumerator();
+        using var otherValues = other.GetAtomicValues().GetEnumerator();
+
+        while (thisValues.MoveNext() && otherValues.MoveNext())
+        {
+            if (thisValues.Current is null ^ otherValues.Current is null)
+            {
+                return false;
+            }
+
+            if (thisValues.Current != null &&
+                !thisValues.Current.Equals(otherValues.Current))
+            {
+                return false;
+            }
+        }
+
+        return !thisValues.MoveNext() && !otherValues.MoveNext();
+    }
+}
