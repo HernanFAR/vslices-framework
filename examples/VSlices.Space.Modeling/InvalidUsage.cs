044dad676bd8d7c159b5ea7bc51ed8c3bffc23eb
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-10 20:17:32 -0300

    modeling: probe immutable state members

--- examples/VSlices.Space.Modeling/InvalidUsage.cs
+++ examples/VSlices.Space.Modeling/InvalidUsage.cs
@@ -3,22 +3,28 @@ namespace VSlices.Space.Modeling;
 
 public static class InvalidUsage
 {
-    public static Location.State CannotMintState(Location.Name name)
+    public static Location.State CannotMintState(LocationName name)
     {
         // Expected compile failure: State construction is owned by Location.
         return new Location.State(name, 0, 0);
     }
 
-    public static Location.Name CannotMintName()
+    public static LocationName CannotMintName()
     {
         // Expected compile failure: Name must be established through its transformation rules.
         return new LocationName("Forged");
     }
 
-    public static void CannotReplaceState(Location location)
+    public static Location.State CannotChangeCreationOnlyName(Location location, LocationName anotherName)
     {
-        // Expected compile failure: accepted state cannot be replaced from outside Location.
-        location.State = location.State with { X = 999 };
+        // Expected compile failure: Name is get-only once the first Location.State is established.
+        return location.CurrentState with { Name = anotherName };
+    }
+
+    public static void CannotReplaceAcceptedState(Location location)
+    {
+        // Expected compile failure: the currently accepted State is get-only on Location.
+        location.CurrentState = location.CurrentState with { X = 999 };
     }
 }
 #endif
