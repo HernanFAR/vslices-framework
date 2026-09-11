194da5a56e01f6a0b93d9c5ef671c32da82e3279
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-10 23:01:33 -0300

    test: exercise Q mass interoperability

---
+++ tests/VSlices.Space.Modeling.Tests/QuantityFamilyModelingTests.cs
@@ -0,0 +1,73 @@
+using System.Numerics;
+using VSlices.Space.Modeling.QuantityFamilies;
+
+namespace VSlices.Space.Modeling.Tests;
+
+public class QuantityFamilyModelingTests
+{
+    [Fact]
+    public void Recommended_mass_is_a_member_of_the_mass_quantity_family()
+    {
+        var mass = new Mass<double>(2.5d);
+
+        Assert.IsAssignableFrom<Q<Dimension.Mass, Grams, Kilo, double>>(mass);
+    }
+
+    [Fact]
+    public void Same_coordinate_mass_addition_uses_the_general_family_operator()
+    {
+        Mass<Grams, Kilo, double> left = new ProbeMass<Grams, Kilo, double>(2d);
+        Mass<Grams, Kilo, double> right = new ProbeMass<Grams, Kilo, double>(3d);
+
+        var result = left + right;
+
+        Assert.Equal(5d, result.Value);
+    }
+
+    [Fact]
+    public void Same_unit_mass_can_add_a_different_prefix_and_backing_type()
+    {
+        var kilograms = new ProbeMass<Grams, Kilo, double>(1d);
+        var micrograms = new ProbeMass<Grams, Micro, decimal>(500_000_000m);
+
+        var result = kilograms.Add(micrograms);
+
+        Assert.Equal(1.5d, result.Value, precision: 12);
+    }
+
+    [Fact]
+    public void Fully_open_mass_can_add_across_unit_prefix_and_backing_type()
+    {
+        var grams = new ProbeMass<Grams, None, decimal>(1_000m);
+        var pounds = new ProbeMass<Pounds, None, double>(1d);
+
+        var result = grams.Add(pounds);
+
+        Assert.Equal(1_453.59237m, result.Value);
+    }
+
+    [Fact]
+    public void Fully_open_mass_addition_is_left_biased()
+    {
+        var pounds = new ProbeMass<Pounds, None, decimal>(1m);
+        var kilograms = new ProbeMass<Grams, Kilo, double>(1d);
+
+        var result = pounds.Add(kilograms);
+
+        var expectedPounds = 1m + (1_000m / Pounds.Scale);
+        Assert.Equal(expectedPounds, result.Value);
+    }
+
+    private sealed class ProbeMass<U, P, T> : Mass<U, P, T>
+        where U : Unit<Dimension.Mass>
+        where P : Prefix
+        where T : INumber<T>
+    {
+        public ProbeMass(T value) : base(value)
+        {
+        }
+
+        protected override Mass<U, P, T> Recreate(T value) =>
+            new ProbeMass<U, P, T>(value);
+    }
+}
